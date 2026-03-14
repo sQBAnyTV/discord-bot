3d733a6dca85c344a59ed2f38668b941a9081ef6
sQBAnyTV <jakub19122002@gmail.com>
2026-03-14 19:03:51 +0100

    Dodano pełne logi do messageReaction

--- events/messageReaction.js
+++ events/messageReaction.js
@@ -1,4 +1,4 @@
-const ReactionRole = require('../models/ReactionRole');
+const ReactionRole = require('../models/reactionrole');
 
 module.exports = (client) => {
     console.log('✅ Rejestruję eventy reakcji...');
